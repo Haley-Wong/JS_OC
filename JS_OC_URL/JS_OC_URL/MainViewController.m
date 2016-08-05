c8261b36e620e5730518a99706fcee13e489b77e
Joker-King <wanghl1113@163.com>
2016-08-05 13:47:29 +0800

    WKWebView和UIWebView拦截URL实现JS 和OC交互的例子。
    WKWebView和UIWebView拦截URL实现JS 和OC交互的例子。

---
+++ JS_OC_URL/JS_OC_URL/MainViewController.m
@@ -0,0 +1,36 @@
+//
+//  MainViewController.m
+//  JS_OC_URL
+//
+//  Created by Harvey on 16/8/4.
+//  Copyright © 2016年 Haley. All rights reserved.
+//
+
+#import "MainViewController.h"
+#import "WebViewController.h"
+#import "WKWebViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    self.title = @"主页";
+}
+
+- (IBAction)btnClick1:(id)sender {
+    WebViewController *webVC = [[WebViewController alloc] init];
+    [self.navigationController pushViewController:webVC animated:YES];
+}
+
+
+- (IBAction)btnClick2:(id)sender {
+    WKWebViewController *wkWebVC = [[WKWebViewController alloc] init];
+    [self.navigationController pushViewController:wkWebVC animated:YES];
+}
+
+@end
---
+++ JS_OC_URL/JS_OC_URL/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  JS_OC_URL
+//
+//  Created by Harvey on 16/8/4.
+//  Copyright © 2016年 Haley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
